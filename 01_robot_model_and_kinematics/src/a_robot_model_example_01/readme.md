b749d0223f49eb8d095c862fc7b658ae0ad841fa
Rojas Rafael <rafael.rojas@unibz.it>
2020-11-08 00:03:39 +0100

    [docker] windows container start with X support

--- 01_robot_model_and_kinematics/src/a_robot_model_example_01/readme.md
+++ 01_robot_model_and_kinematics/src/a_robot_model_example_01/readme.md
@@ -1,5 +1,14 @@
 # Robot Model, Robot State, Robot Model Loader and Robot Inverce and Direct Kinematics
 
+## Robot Model Loader
+
+MoveIt provedes an interface to load models a `robot_model_loader::RobotModelLoader` [defined here](https://github.com/ros-planning/moveit/blob/802e596a2283b64f4582d802c5f79e1f3d57def0/moveit_ros/planning/robot_model_loader/include/moveit/robot_model_loader/robot_model_loader.h) and [implemented here](https://github.com/ros-planning/moveit/blob/7ad2bc7b86dad08061d98668ba34feba54bb05cc/moveit_ros/planning/robot_model_loader/src/robot_model_loader.cpp).
+
+- [`moveit_ros/planning/rdf_loader`]
+    - `rdf_loader::RDFLoader` [defined here](https://github.com/ros-planning/moveit/blob/802e596a2283b64f4582d802c5f79e1f3d57def0/moveit_ros/planning/rdf_loader/include/moveit/rdf_loader/rdf_loader.h) and [implemented here](https://github.com/ros-planning/moveit/blob/cb5945b69f4dfbd1e5819a2b7bee3e256aee766c/moveit_ros/planning/rdf_loader/src/rdf_loader.cpp). Loads the contnet of `"robot_description"` with `rdf_loader::RDFLoader::initString`. and Loads the contnet of `"robot_description_semantic"` with `srdf::Model::initString`.
+    
+
+
 ## Robot Model
 
 The model of the robot is wraped in the class `moveit::core::RobotModel` [defined here](https://github.com/ros-planning/moveit/blob/a29a30caaecbd130d85056d959d4eb1c30d4088f/moveit_core/robot_model/include/moveit/robot_model/robot_model.h) and [implemented here](https://github.com/ros-planning/moveit/blob/a29a30caaecbd130d85056d959d4eb1c30d4088f/moveit_core/robot_model/src/robot_model.cpp).
@@ -24,6 +33,13 @@ The model of the robot is wraped in the class `moveit::core::RobotModel` [define
     - `urdf::Mesh`
     - etc.
 
+- [`srdf`](https://wiki.ros.org/srdf)
+    - `srdf::Model`
+
+```
+  RobotModelLoader(const Options& opt = Options());
+  RobotModelLoader(const std::string& robot_description, bool load_kinematics_solvers = true);
+```
 
 # Methods used in this example
 
@@ -39,3 +55,27 @@ classes.
                                                                  
 We will start by instantiating a
 `RobotModelLoader` object, which will look up the robot description on the ROS parameter server and construct a
+
+## SRDF
+
+The SRDF or Semantic Robot Description Format complement the URDF and specifies joint groups, default robot configurations, additional collision checking information, and additional transforms that may be needed to completely specify the robot’s pose. 
+The recommended way to generate a SRDF is using the MoveIt! Setup Assistant.
+
+- **Virtual Joints** are additional joints need to be defined to specify the pose of the root link of the robot with respect to a world coordinate system. E.g., a mobile robot is specified using a planar virtual joint that attaches the world coordinate frame to the frame of the robot. A fixed robot should be attached to the world using a fixed joint.
+
+- **Passive Joints** are unactuated joints on a robot, e.g. passive casters on a differential drive robots. They are specified separately in the SRDF to make sure that different components in the motion planning or control pipelines know that the joints cannot be directly controlled.
+
+- **Group** (sometimes called **JointGroup** or **Planning Group**) is a central concept in MoveIt. MoveIt! will only consider moving the joints in the group that it is planning for and the other joints are left stationary. A group is simply a collection of joints and links. Each group can be specified in one of several different ways:
+    - **Collection of Joints** All the child links of each joint are automatically included in the group.
+    - **Collection of Links** All the parent joints of the links are also included in the group.
+    - **Serial Chain**   serial chain is specified using the base link and the tip link. The tip link in a chain is the child link of the last joint in the chain. The base link in a chain is the parent link for the first joint in the chain.
+Collection of Sub-Groups
+    - **collection of groups**
+
+- **End-Effectors** Certain groups in a robot can be given a special designation as an end-effector. An end-effector is typically connected to another group (like an arm) through a fixed joint. Note that when specifying groups that are end-effectors, it's important to make sure that there are no common links between the end-effector and the parent group it is connected to.
+
+- **Self-Collisions** The Default Self-Collision Matrix Generator is part of the Setup Assistant. It searches for pairs of links on the robot that can safely be disabled from collision checking, decreasing motion planning processing time. These pairs of links are disabled when they are always in collision, never in collision, in collision in the robot’s default position or when the links are adjacent to each other on the kinematic chain. The sampling density specifies how many random robot positions to check for self collision. Higher densities require more computation time while lower densities have a higher possibility of disabling pairs that should not be disabled. The default value is 10,000 collision checks. Collision checking is done in parallel to decrease processing time.
+
+- **Robot Poses** The SRDF can also store fixed configurations of the robot. 
+
+
--- 01_robot_model_and_kinematics/src/a_robot_model_example_01/src/a_robot_model_example_01_node.cpp
+++ 01_robot_model_and_kinematics/src/a_robot_model_example_01/src/a_robot_model_example_01_node.cpp
@@ -30,92 +30,92 @@ int main(int argc, char** argv)
   robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
   const moveit::core::RobotModelPtr& kinematic_model = robot_model_loader.getModel();
   ROS_INFO("Model frame: %s", kinematic_model->getModelFrame().c_str());
-
-  // Using the :moveit_core:`RobotModel`, we can construct a
-  // :moveit_core:`RobotState` that maintains the configuration
-  // of the robot. We will set all joints in the state to their
-  // default values. We can then get a
-  // :moveit_core:`JointModelGroup`, which represents the robot
-  // model for a particular group, e.g. the "panda_arm" of the Panda
-  // robot.
-  moveit::core::RobotStatePtr kinematic_state(new moveit::core::RobotState(kinematic_model));
-  kinematic_state->setToDefaultValues();
-  const moveit::core::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("panda_arm");
-
-  const std::vector<std::string>& joint_names = joint_model_group->getVariableNames();
-
-  // Get Joint Values
-  // ^^^^^^^^^^^^^^^^
-  // We can retreive the current set of joint values stored in the state for the Panda arm.
-  std::vector<double> joint_values;
-  kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
-  for (std::size_t i = 0; i < joint_names.size(); ++i)
-  {
-    ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
-  }
-
-  // Joint Limits
-  // ^^^^^^^^^^^^
-  // setJointGroupPositions() does not enforce joint limits by itself, but a call to enforceBounds() will do it.
-  /* Set one joint in the Panda arm outside its joint limit */
-  joint_values[0] = 5.57;
-  kinematic_state->setJointGroupPositions(joint_model_group, joint_values);
-
-  /* Check whether any joint is outside its joint limits */
-  ROS_INFO_STREAM("Current state is " << (kinematic_state->satisfiesBounds() ? "valid" : "not valid"));
-
-  /* Enforce the joint limits for this state and check again*/
-  kinematic_state->enforceBounds();
-  ROS_INFO_STREAM("Current state is " << (kinematic_state->satisfiesBounds() ? "valid" : "not valid"));
-
-  // Forward Kinematics
-  // ^^^^^^^^^^^^^^^^^^
-  // Now, we can compute forward kinematics for a set of random joint
-  // values. Note that we would like to find the pose of the
-  // "panda_link8" which is the most distal link in the
-  // "panda_arm" group of the robot.
-  kinematic_state->setToRandomPositions(joint_model_group);
-  const Eigen::Isometry3d& end_effector_state = kinematic_state->getGlobalLinkTransform("panda_link8");
-
-  /* Print end-effector pose. Remember that this is in the model frame */
-  ROS_INFO_STREAM("Translation: \n" << end_effector_state.translation() << "\n");
-  ROS_INFO_STREAM("Rotation: \n" << end_effector_state.rotation() << "\n");
-
-  // Inverse Kinematics
-  // ^^^^^^^^^^^^^^^^^^
-  // We can now solve inverse kinematics (IK) for the Panda robot.
-  // To solve IK, we will need the following:
-  //
-  //  * The desired pose of the end-effector (by default, this is the last link in the "panda_arm" chain):
-  //    end_effector_state that we computed in the step above.
-  //  * The timeout: 0.1 s
-  double timeout = 0.1;
-  bool found_ik = kinematic_state->setFromIK(joint_model_group, end_effector_state, timeout);
-
-  // Now, we can print out the IK solution (if found):
-  if (found_ik)
-  {
-    kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
-    for (std::size_t i = 0; i < joint_names.size(); ++i)
-    {
-      ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
-    }
-  }
-  else
-  {
-    ROS_INFO("Did not find IK solution");
-  }
-
-  // Get the Jacobian
-  // ^^^^^^^^^^^^^^^^
-  // We can also get the Jacobian from the :moveit_core:`RobotState`.
-  Eigen::Vector3d reference_point_position(0.0, 0.0, 0.0);
-  Eigen::MatrixXd jacobian;
-  kinematic_state->getJacobian(joint_model_group,
-                               kinematic_state->getLinkModel(joint_model_group->getLinkModelNames().back()),
-                               reference_point_position, jacobian);
-  ROS_INFO_STREAM("Jacobian: \n" << jacobian << "\n");
-  // END_TUTORIAL
+////
+////  // Using the :moveit_core:`RobotModel`, we can construct a
+////  // :moveit_core:`RobotState` that maintains the configuration
+////  // of the robot. We will set all joints in the state to their
+////  // default values. We can then get a
+////  // :moveit_core:`JointModelGroup`, which represents the robot
+////  // model for a particular group, e.g. the "panda_arm" of the Panda
+////  // robot.
+////  moveit::core::RobotStatePtr kinematic_state(new moveit::core::RobotState(kinematic_model));
+////  kinematic_state->setToDefaultValues();
+////  const moveit::core::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("panda_arm");
+////
+////  const std::vector<std::string>& joint_names = joint_model_group->getVariableNames();
+////
+////  // Get Joint Values
+////  // ^^^^^^^^^^^^^^^^
+////  // We can retreive the current set of joint values stored in the state for the Panda arm.
+////  std::vector<double> joint_values;
+////  kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
+////  for (std::size_t i = 0; i < joint_names.size(); ++i)
+////  {
+////    ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
+////  }
+////
+////  // Joint Limits
+////  // ^^^^^^^^^^^^
+////  // setJointGroupPositions() does not enforce joint limits by itself, but a call to enforceBounds() will do it.
+////  /* Set one joint in the Panda arm outside its joint limit */
+////  joint_values[0] = 5.57;
+////  kinematic_state->setJointGroupPositions(joint_model_group, joint_values);
+////
+////  /* Check whether any joint is outside its joint limits */
+////  ROS_INFO_STREAM("Current state is " << (kinematic_state->satisfiesBounds() ? "valid" : "not valid"));
+////
+////  /* Enforce the joint limits for this state and check again*/
+////  kinematic_state->enforceBounds();
+////  ROS_INFO_STREAM("Current state is " << (kinematic_state->satisfiesBounds() ? "valid" : "not valid"));
+////
+////  // Forward Kinematics
+////  // ^^^^^^^^^^^^^^^^^^
+////  // Now, we can compute forward kinematics for a set of random joint
+////  // values. Note that we would like to find the pose of the
+////  // "panda_link8" which is the most distal link in the
+////  // "panda_arm" group of the robot.
+////  kinematic_state->setToRandomPositions(joint_model_group);
+////  const Eigen::Isometry3d& end_effector_state = kinematic_state->getGlobalLinkTransform("panda_link8");
+////
+////  /* Print end-effector pose. Remember that this is in the model frame */
+////  ROS_INFO_STREAM("Translation: \n" << end_effector_state.translation() << "\n");
+////  ROS_INFO_STREAM("Rotation: \n" << end_effector_state.rotation() << "\n");
+////
+////  // Inverse Kinematics
+////  // ^^^^^^^^^^^^^^^^^^
+////  // We can now solve inverse kinematics (IK) for the Panda robot.
+////  // To solve IK, we will need the following:
+////  //
+////  //  * The desired pose of the end-effector (by default, this is the last link in the "panda_arm" chain):
+////  //    end_effector_state that we computed in the step above.
+////  //  * The timeout: 0.1 s
+////  double timeout = 0.1;
+////  bool found_ik = kinematic_state->setFromIK(joint_model_group, end_effector_state, timeout);
+////
+////  // Now, we can print out the IK solution (if found):
+////  if (found_ik)
+////  {
+////    kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
+////    for (std::size_t i = 0; i < joint_names.size(); ++i)
+////    {
+////      ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
+////    }
+////  }
+////  else
+////  {
+////    ROS_INFO("Did not find IK solution");
+////  }
+////
+////  // Get the Jacobian
+////  // ^^^^^^^^^^^^^^^^
+////  // We can also get the Jacobian from the :moveit_core:`RobotState`.
+////  Eigen::Vector3d reference_point_position(0.0, 0.0, 0.0);
+////  Eigen::MatrixXd jacobian;
+////  kinematic_state->getJacobian(joint_model_group,
+////                               kinematic_state->getLinkModel(joint_model_group->getLinkModelNames().back()),
+////                               reference_point_position, jacobian);
+////  ROS_INFO_STREAM("Jacobian: \n" << jacobian << "\n");
+////  // END_TUTORIAL
 
   ros::shutdown();
   return 0;
