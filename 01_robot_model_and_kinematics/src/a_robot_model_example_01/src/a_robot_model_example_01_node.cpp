8b8bd39875c8b03a6540d6ecf36015eb4714328f
Rojas Rafael <rafael.rojas@unibz.it>
2020-11-17 08:44:36 +0100

    [01, 02] more documentation

--- 01_robot_model_and_kinematics/src/a_robot_model_example_01/src/a_robot_model_example_01_node.cpp
+++ 01_robot_model_and_kinematics/src/a_robot_model_example_01/src/a_robot_model_example_01_node.cpp
@@ -1,69 +1,99 @@
 /* Author: Rafael A. Rojas*/
 /*ros::init
-  ros::AsyncSpinner 
+  ros::AsyncSpinner
  * */
 #include <ros/ros.h>
 
 // MoveIt
-#include <moveit/robot_model_loader/robot_model_loader.h> // robot_model_loader::RobotModelLoader
 #include <moveit/robot_model/robot_model.h> //robot_model::RobotModel
+#include <moveit/robot_model_loader/robot_model_loader.h> // robot_model_loader::RobotModelLoader
 #include <moveit/robot_state/robot_state.h>
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
   ros::init(argc, argv, "a_robot_model_example_01");
   ros::NodeHandle nh;
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
-  for(const std::string& param :{"robot_description", "robot_description_semantic"})
-    if(!nh.hasParam(param)){
+  for (const std::string &param :
+       {"robot_description", "robot_description_semantic"})
+    if (!nh.hasParam(param)) {
       ROS_ERROR("ROS paramter \"%s\" is not set", param.c_str());
       ros::shutdown();
       return 0;
     }
-  robot_model_loader::RobotModelLoader my_robot_model_loader("robot_description");
+  robot_model_loader::RobotModelLoader my_robot_model_loader(
+      "robot_description");
   ROS_INFO("robot_model_loader::RobotModelLoader instantiated");
-  const robot_model::RobotModelPtr& my_robot_model = my_robot_model_loader.getModel();
+  const robot_model::RobotModelPtr &my_robot_model =
+      my_robot_model_loader.getModel();
   ROS_INFO("robot_model_loader::RobotModelLoader instantiated");
   ROS_INFO("Model frame: %s", my_robot_model->getModelFrame().c_str());
 
   /*Get the names of all groups that are defined for this mode*/
   {
-    const std::vector<std::string>& group_names = my_robot_model->getJointModelGroupNames();
+    const std::vector<std::string> &group_names =
+        my_robot_model->getJointModelGroupNames();
     ROS_INFO("-- Groups in this model ");
-    for(const std::string& gname :group_names){
+    for (const std::string &gname : group_names) {
       bool is_end_effector = my_robot_model->hasEndEffector(gname);
-      ROS_INFO("    %s which is %s an end-effector", gname.c_str(), is_end_effector?"":"not");
-      const moveit::core::JointModelGroup* joint_model_group = my_robot_model->getJointModelGroup(gname);
+      ROS_INFO("    %s which is %s an end-effector", gname.c_str(),
+               is_end_effector ? "" : "not");
+      const moveit::core::JointModelGroup *joint_model_group =
+          my_robot_model->getJointModelGroup(gname);
       ROS_INFO("    -- variable (join) names in the group %s", gname.c_str());
-      const std::vector<std::string>& variable_names = joint_model_group->getVariableNames();
-      for(const std::string& vname :variable_names)
+      const std::vector<std::string> &variable_names =
+          joint_model_group->getVariableNames();
+      for (const std::string &vname : variable_names)
         ROS_INFO("        %s", vname.c_str());
       ROS_INFO("    -- link names in the group %s", gname.c_str());
-      const std::vector<std::string>& link_names = joint_model_group->getLinkModelNames();
-      for(const std::string& lname :link_names)
+      const std::vector<std::string> &link_names =
+          joint_model_group->getLinkModelNames();
+      for (const std::string &lname : link_names)
         ROS_INFO("        %s", lname.c_str());
     }
   }
 
-  robot_state::RobotStatePtr my_kinematic_model(new robot_state::RobotState(my_robot_model));
+  // Test the moveit kinematic model (called robot state).
+  // WHwere we get all the "groups" in the robot model. These groups are
+  // kinematic chains. They have joints and links.
+  // The next loop does the following:
+  // 1. instantiate a vector of strings with the names of the joint groups in
+  // the model
+  // 2. for seach joint group in the robot model do
+  //    1. get the names of the joints inside the group
+  //    2. for each
+  robot_state::RobotStatePtr my_kinematic_model(
+      new robot_state::RobotState(my_robot_model));
   {
-    const std::vector<std::string>& group_names = my_robot_model->getJointModelGroupNames();
-    for(const std::string& gname :group_names){
-      const moveit::core::JointModelGroup* joint_model_group = my_kinematic_model->getJointModelGroup(gname);
-      const std::vector<std::string>& variable_names = joint_model_group->getVariableNames();
-      const std::vector<std::string>& link_names = joint_model_group->getLinkModelNames();
+    // 1. get groups in the robot model
+    const std::vector<std::string> &group_names =
+        my_robot_model->getJointModelGroupNames();
+    for (const std::string &gname : group_names) {
+      // 2. instantiate the group of joints
+      const moveit::core::JointModelGroup *joint_model_group =
+          my_kinematic_model->getJointModelGroup(gname);
+      // 3. The variables are the joints. Get the joint names.
+      const std::vector<std::string> &variable_names =
+          joint_model_group->getVariableNames();
+      // 4. get the links inside the group
+      const std::vector<std::string> &link_names =
+          joint_model_group->getLinkModelNames();
+      // 5. get the actual position of the joints
       std::vector<double> joint_values;
-      my_kinematic_model->copyJointGroupPositions(joint_model_group, joint_values);
+      my_kinematic_model->copyJointGroupPositions(joint_model_group,
+                                                  joint_values);
       ROS_INFO("-- joints in group %s with their position", gname.c_str());
       for (std::size_t i = 0; i < variable_names.size(); ++i)
         ROS_INFO("Joint %s: %f", variable_names[i].c_str(), joint_values[i]);
-      for(const std::string& lname :link_names){
+      for (const std::string &lname : link_names) {
         // getGlobalLinkTransform calls updateLinkTransforms
-        const Eigen::Isometry3d& link_pose = my_kinematic_model->getGlobalLinkTransform(lname);
-        ROS_INFO_STREAM(lname << " Translation: \n" << link_pose.translation() << "\n");
-        ROS_INFO_STREAM(lname << " Rotation: \n" << link_pose.rotation() << "\n");
+        const Eigen::Isometry3d &link_pose =
+            my_kinematic_model->getGlobalLinkTransform(lname);
+        ROS_INFO_STREAM(lname << " Translation: \n"
+                              << link_pose.translation() << "\n");
+        ROS_INFO_STREAM(lname << " Rotation: \n"
+                              << link_pose.rotation() << "\n");
       }
     }
   }
--- 02_planning_scene/src/a_planning_scene_class/src/a_planning_scene_class_node.cpp
+++ 02_planning_scene/src/a_planning_scene_class/src/a_planning_scene_class_node.cpp
@@ -1,25 +1,24 @@
 
 /* Author: Rafael A. Rojas*/
 
-#include <ros/ros.h> // ros::init, ros::AsyncSpinner, 
+#include <ros/ros.h> // ros::init, ros::AsyncSpinner,
 #include <sensor_msgs/JointState.h>
 
 // MoveIt
-#include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/planning_scene/planning_scene.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
 
 #include <moveit/kinematic_constraints/utils.h>
 
-
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
   ros::init(argc, argv, "a_planning_scene_class_example");
   ros::AsyncSpinner spinner(1);
   spinner.start();
   std::size_t count = 0;
   ros::NodeHandle nh;
 
-  ros::Publisher js_pub = nh.advertise<sensor_msgs::JointState>("joint_states_cmd", 1);
+  ros::Publisher js_pub =
+      nh.advertise<sensor_msgs::JointState>("joint_states_cmd", 1);
   sensor_msgs::JointState js;
   // 1) instantiate the  MoveIt robot model loader
   robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
@@ -33,24 +32,37 @@ int main(int argc, char** argv)
   collision_request.max_contacts = 1000;
   collision_detection::CollisionResult collision_result;
 
-  while(ros::ok()){
-    robot_state::RobotState& kinematic_model_actual = planning_scene.getCurrentStateNonConst();
-    const robot_model::JointModelGroup* joint_group = kinematic_model_actual.getJointModelGroup("myrobotplanninggroup");
+  while (ros::ok()) {
+    // 4. get the robot state and set the joints of the planning group to a
+    // random position
+    robot_state::RobotState &kinematic_model_actual =
+        planning_scene.getCurrentStateNonConst();
+    // 4.1 instantiate the planning group. The name myrobotplanninggroup was
+    // defined with he moveit setup assistant
+    const robot_model::JointModelGroup *joint_group =
+        kinematic_model_actual.getJointModelGroup("myrobotplanninggroup");
+    // 4.2 set the joint positions of the planning grou to a random position
+    kinematic_model_actual.setToRandomPositions(joint_group);
+
+    // 5. set the values of the planning request and the planning result
     collision_request.group_name = "myrobotplanninggroup";
     collision_result.clear();
-    kinematic_model_actual.setToRandomPositions(joint_group);
+    // 6. call the collision service (in this example this will call the FCL)
     planning_scene.checkSelfCollision(collision_request, collision_result);
 
+    // 7. retrieve the random joint poisition and publish to the topic
     kinematic_model_actual.copyJointGroupPositions(joint_group, js.position);
     js.name = joint_group->getVariableNames();
     js_pub.publish(js);
 
-    if(collision_result.collision){
+    if (collision_result.collision) {
       ROS_INFO_STREAM("A COLLISION WAS DETECTED");
       collision_detection::CollisionResult::ContactMap::const_iterator it;
-      for (it = collision_result.contacts.begin(); it != collision_result.contacts.end(); ++it)
-        ROS_INFO("    Contact between: %s and %s", it->first.first.c_str(), it->first.second.c_str());
-    }else
+      for (it = collision_result.contacts.begin();
+           it != collision_result.contacts.end(); ++it)
+        ROS_INFO("    Contact between: %s and %s", it->first.first.c_str(),
+                 it->first.second.c_str());
+    } else
       ROS_INFO_STREAM("Position feasible");
 
     ros::WallDuration(4).sleep();
