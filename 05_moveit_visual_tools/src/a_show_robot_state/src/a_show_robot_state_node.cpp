434042d84908422f36a5c48124f6b145cbcb52e6
Rojas Rafael <rafael.rojas@unibz.it>
2021-03-04 16:49:22 +0100

    [05/doc]

--- 05_moveit_visual_tools/src/a_show_robot_state/src/a_show_robot_state_node.cpp
+++ 05_moveit_visual_tools/src/a_show_robot_state/src/a_show_robot_state_node.cpp
@@ -8,28 +8,43 @@
 #include <moveit/robot_model/robot_model.h> //robot_model::RobotModel
 #include <moveit/robot_model_loader/robot_model_loader.h> // robot_model_loader::RobotModelLoader
 #include <moveit/robot_state/robot_state.h>
+#include <moveit_msgs/PlanningScene.h> //planning_scene::PlanningScene
+#include <moveit_visual_tools/moveit_visual_tools.h>
 
 class ShowRobotState {
 public:
   ShowRobotState()
       : loader_("robot_description"), model_(loader_.getModel()),
-        workspace_(new planning_scene::PlanningScene(model_)),
-        visual_tool_("table") {
-    visual_tools.loadRobotStatePub("/display_robot_state");
-    visual_tools.enableBatchPublishing();
-    visual_tools.deleteAllMarkers(); // clear all old markers
-    visual_tools.trigger()
+        workspace_(model_), visualizer01_("table"), visualizer02_("table") {
+    // instantiate a  a publisher of moveit_msgs::DisplayRobotState to the
+    // /display_robot_state topic
+    visualizer01_.loadRobotStatePub("display_robot_state_1");
+    visualizer02_.loadRobotStatePub("display_robot_state_2");
   }
-  virtual ~ShowRobotState() {}
+
+  ~ShowRobotState() {}
+
   void spin() {
+    robot_state::RobotState &state = workspace_.getCurrentStateNonConst();
+    const robot_model::JointModelGroup *joint_group =
+        model_->getJointModelGroup(model_->getJointModelGroupNames().back());
+    visualizer01_.setAlpha(0.1);
+    visualizer02_.setAlpha(0.5);
     while (ros::ok()) {
+      state.setToRandomPositions(joint_group);
+      visualizer01_.publishRobotState(state, rviz_visual_tools::GREEN);
+      state.setToRandomPositions(joint_group);
+      visualizer02_.publishRobotState(state, rviz_visual_tools::RED);
+      // visualizer01_.trigger();
+      ros::WallDuration(1).sleep();
       ros::spinOnce();
     }
   }
   robot_model_loader::RobotModelLoader loader_;
   const robot_model::RobotModelPtr model_;
-  planning_scene::PlanningScenePtr workspace_;
-  moveit_visual_tools::MoveItVisualTools visual_tool_;
+  planning_scene::PlanningScene workspace_;
+  moveit_visual_tools::MoveItVisualTools visualizer01_;
+  moveit_visual_tools::MoveItVisualTools visualizer02_;
 };
 
 int main(int argc, char **argv) {
@@ -37,7 +52,9 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "a_show_robot_state_node");
   ros::NodeHandle nh;
 
-  // ros::shutdown();
-  // ros::waitForShutdown();
+  ShowRobotState srs;
+
+  srs.spin();
+
   return 0;
 }
